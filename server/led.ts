37d63d518a784aa57e42c235d45f336e199495c0
Dimitrios Dentsas <d.dentsas@googlemail.com>
2019-12-13 01:26:00 +0100

    create led library

--- server/led.ts
+++ server/led.ts
@@ -1,55 +1,94 @@
-import {Board, Led} from 'johnny-five';
-import {Strip, FORWARD} from 'node-pixel';
+import { Board } from 'johnny-five';
+import { Strip } from 'node-pixel';
 
-const board = new Board();
+function sleep(ms): Promise<any> {
+  return new Promise((resolve): any => setTimeout(resolve, ms))
+}
 
-export { triggerLed }
+async function wait(ms): Promise<any> {
+  await sleep(ms)
+}
 
-function triggerLed(): void {
-    board.on("ready", function() {
-        // Define our hardware.
-        // It's a 12px ring connected to pin 6.
-        var strip = new Strip({
-          board: this,
+export enum Direction {
+  LEFT = 1,
+  RIGHT
+}
+
+interface SpinOptions {
+  direction: Direction;
+  color: string, // https://github.com/Qix-/color-string
+  speed: number, // fps
+  loops: number
+}
+
+class Led {
+  private strip: Strip
+  private board: Board  
+
+  public constructor() {
+    this.board = new Board();
+    this.board.on("ready", () => {      
+      this.strip = new Strip({
+          board: this.board,
           controller: "FIRMATA",
           strips: [ {pin: 6, length: 24}, ],
           gamma: 2.8,
-        });
-      
-        // Just like DOM-ready for web developers.
-        strip.on("ready", function() {
-          strip.clear()
-          // Set the entire strip to pink.
-          //strip.color('#903');
-      
-          //var color = Color.rgb(0, 50, 0)
-          //color.lighten(0.5)
-          
-          var pos = 0;
-          var loopCounter = 0
-          var abortAfterLoops = 3
-    
-          var loop = setInterval(function () {                
-    
-            if (++pos >= strip.length) {
-                loopCounter++;
-                pos = 0;
-                strip.clear()
-            }        
-    
-            // Shift all pixels clockwise
-            //strip.shift(1, FORWARD, true);
-            strip.pixel(pos).color([50, 0, 0]);
-            strip.show();
-    
-            if (loopCounter == abortAfterLoops) {
-                clearInterval(this)
-                strip.clear()
-            }            
-    
-          }, 1000 / 12); 
-    
-        });
-      
       });
+      console.log("strip initialized")
+    });
+  }
+
+  public stopLed(): void {
+    this.strip.clear()
+  }
+
+  public leftSpin(): void {
+    this.spin({
+      direction: Direction.LEFT,
+      color: "rgb(0, 0, 50)",
+      speed: 12, // fps
+      loops: 2
+    });
+  }
+
+  private async spin(options: SpinOptions): Promise<void> {
+
+    this.strip.clear()
+   
+    var pos = 0;
+    var loopCounter = 0
+    var abortAfterLoops = options.loops
+
+    this.strip.pixel(pos).color(options.color);
+    this.strip.show();
+
+    console.log(this.strip.length)
+
+    while (loopCounter !== abortAfterLoops ) {
+      let calculatedPos: number = pos;
+
+      if (++pos >= this.strip.length || calculatedPos >= this.strip.length) {
+          loopCounter++;
+          pos = 0;
+          this.strip.clear()
+      } 
+            
+      if (options.direction == Direction.LEFT)
+        calculatedPos = this.strip.length - pos
+
+      console.log(calculatedPos)
+
+      this.strip.pixel(calculatedPos).color(options.color);
+      this.strip.show();
+
+      await wait( 1000 / options.speed)
+    }
+    
+    this.strip.clear()
+
+    return;
+  }
+
 }
+
+export { Led }
--- server/server.ts
+++ server/server.ts
@@ -1,58 +1,16 @@
-const express = require( "express" );
-import { Board } from 'johnny-five';
-import { Strip } from 'node-pixel';
+import express from 'express';
+import { Led } from './led'
 
 const app = express();
-const board = new Board();
-var strip;
-
-board.on("ready", function() {
-    strip = new Strip({
-        board: board,
-        controller: "FIRMATA",
-        strips: [ {pin: 6, length: 24}, ],
-        gamma: 2.8,
-    });
-
-    strip.on("error", function () {
-        console.log("error")
-    }) 
-});
+const led = new Led();
 
 app.get('/', (req, res) => {
     res.send({});
 })
 
-app.get('/api/led', (req, res) => {       
-    strip.clear()
-   
-    var pos = 0;
-    var loopCounter = 0
-    var abortAfterLoops = 3
-
-    strip.pixel(pos).color([50, 0, 0]);
-    strip.show();
-
-    var loop = setInterval(function () {                
-
-    if (++pos >= strip.length) {
-        loopCounter++;
-        pos = 0;
-        strip.clear()
-    }        
-
-    // Shift all pixels clockwise
-    //strip.shift(1, FORWARD, true);
-    strip.pixel(pos).color([50, 0, 0]);
-    strip.show();
-
-    if (loopCounter == abortAfterLoops) {
-        clearInterval(this)
-        strip.clear()
-        res.sendStatus(200);
-    }            
-
-    }, 1000 / 12); 
+app.get('/api/led', (req, res) => {      
+    led.leftSpin();
+    res.sendStatus(200)
 })
 
 app.listen(4201, '127.0.0.1', function() {
