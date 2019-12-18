8571d2e578e5088ca9e276d3e56157e8f9ede00f
Dimitrios Dentsas <d.dentsas@googlemail.com>
2019-12-18 22:15:27 +0100

    switch to child_process exec for composite

--- server/imagecompositor.ts
+++ server/imagecompositor.ts
@@ -1,4 +1,4 @@
-const gm = require('gm').subClass({imageMagick: true});
+var exec = require('child_process').exec
 
 class ImageCompositor {
 
@@ -7,6 +7,21 @@ class ImageCompositor {
   }
 
   public composite(): void {
+    var command = [
+      'composite',      
+      'src/assets/templates/overlay.png',
+      'src/assets/stockphoto.jpg', //input
+      '-gravity', 'north',
+      '-geometry', '+0+40', 
+      'built/test.jpg'  //output
+      ];
+
+    exec(command.join(' '), function(err, stdout, stderr) { 
+      if (err) throw err
+      console.log(err);
+    });
+
+    /*
     gm()
     //.command("composite")    
     .in('-gravity', 'north')
@@ -18,7 +33,7 @@ class ImageCompositor {
       if (err) throw err
       console.log(err);
     });
-
+    */
     return;
   }
 
