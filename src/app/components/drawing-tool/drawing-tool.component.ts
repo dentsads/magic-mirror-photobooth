7d7743663391e4f04bafb4afa1a41eed14e9f5d6
Dimitrios Dentsas <d.dentsas@googlemail.com>
2020-02-26 22:48:31 +0100

    issue-10 add custom, non-native color picker

--- src/app/components/drawing-tool/drawing-tool.component.ts
+++ src/app/components/drawing-tool/drawing-tool.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { RoutingService } from '../../services/routing.service';
 import { fabric } from 'fabric';
+import Pickr from '@simonwep/pickr';
 
 @Component({
   selector: 'app-drawing-tool',
   templateUrl: './drawing-tool.component.html',
-  styleUrls: ['./drawing-tool.component.css']
+  styleUrls: ['./drawing-tool.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DrawingToolComponent implements OnInit, OnDestroy {
 
@@ -42,6 +44,57 @@ export class DrawingToolComponent implements OnInit, OnDestroy {
     this.canvas.freeDrawingBrush.shadow.blur = this.drawingShadowWidth;
 
     this.canvas.renderAll();
+
+    // Color picker
+    const pickr = Pickr.create({
+      el: '.color-picker',
+      theme: 'nano', // or 'monolith', or 'nano'
+      comparison: false,
+      default: this.drawingShadowColor,
+      swatches: [
+          'rgba(244, 67, 54, 1)',
+          'rgba(233, 30, 99, 1)',
+          'rgba(156, 39, 176, 1)',
+          'rgba(103, 58, 183, 1)',
+          'rgba(63, 81, 181, 1)',
+          'rgba(33, 150, 243, 1)',
+          'rgba(3, 169, 244, 1)',
+          'rgba(0, 188, 212, 1)',
+          'rgba(0, 150, 136, 1)',
+          'rgba(76, 175, 80, 1)',
+          'rgba(139, 195, 74, 1)',
+          'rgba(205, 220, 57, 1)',
+          'rgba(255, 235, 59, 1)',
+          'rgba(255, 193, 7, 1)'
+      ],
+
+      components: {
+
+          // Main components
+          preview: false,
+          opacity: false,
+          hue: false,
+
+          // Input / output Options
+          interaction: {
+              hex: false,
+              rgba: false,
+              hsla: false,
+              hsva: false,
+              cmyk: false,
+              input: false,
+              clear: false,
+              save: false
+          }
+      }
+    });
+
+    pickr.on('swatchselect', (color, instance) => {
+      pickr.applyColor(false)
+      pickr.hide()
+      this.handleDrawingShadowColorElChange(color.toRGBA().toString())
+    });
+
   }
 
   async ngOnDestroy() {
@@ -59,8 +112,8 @@ export class DrawingToolComponent implements OnInit, OnDestroy {
     this.canvas.clear();
   }
 
-  handleDrawingShadowColorElChange(event) {
-    this.drawingShadowColor = event.target.value;
+  handleDrawingShadowColorElChange(color) {
+    this.drawingShadowColor = color;
     this.canvas.freeDrawingBrush.shadow.color = this.drawingShadowColor;
   }
 
