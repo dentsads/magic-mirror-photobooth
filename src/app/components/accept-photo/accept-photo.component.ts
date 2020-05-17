025095d188acc56babf155ad56abe6f9133b3c34
Dimitrios Dentsas <d.dentsas@googlemail.com>
2020-05-17 10:55:58 +0200

    Chrome v81 now respects EXIF image orientation in browser, so removed manual adjustment code

--- src/app/components/accept-photo/accept-photo.component.ts
+++ src/app/components/accept-photo/accept-photo.component.ts
@@ -15,14 +15,6 @@ export class AcceptPhotoComponent implements OnInit, OnDestroy {
   componentData: any;
   document;
 
-  // see: https://www.exif.org/Exif2-2.PDF page 18
-  private readonly rotation = {
-    1: 'rotate(0deg)',
-    3: 'rotate(180deg)',
-    6: 'rotate(90deg)',
-    8: 'rotate(270deg)'
-  };
-
   constructor(
     public router: Router,
     private activatedRoute: ActivatedRoute,
@@ -53,31 +45,6 @@ export class AcceptPhotoComponent implements OnInit, OnDestroy {
   handleImage() {
     // fade in image
     this.document.getElementById('overlay-outer').style.opacity = '1';    
-
-    // see: https://www.exif.org/Exif2-2.PDF
-    const exifOrientation = this.componentData.context.exifOrientation;
-    this.document.getElementsByClassName('image')[0].style.transform = this.rotation[exifOrientation];
-
-    /*
-      If there is a CSS transform rotation, the document flow of the parent divs is not getting adjusted accordingly.
-      We need to therefore manually change the layout of the parent elements to match the tranformed image layout.
-
-      see:
-       * https://stackoverflow.com/questions/31962378/parent-div-to-change-dimension-when-inner-image-rotate
-       * https://stackoverflow.com/questions/16301625/rotated-elements-in-css-that-affect-their-parents-height-correctly
-    */
-    if (exifOrientation !== 1) {
-      let bound = this.document.getElementsByClassName('image')[0].getBoundingClientRect();
-
-      this.document.getElementById('overlay-outer').style.height = bound.height + 'px'
-      this.document.getElementById('overlay-outer').style.width = bound.width + 'px'
-
-      this.document.getElementsByClassName('image')[0].style['margin-left'] = '-' + (bound.height/2) + 'px'
-      this.document.getElementsByClassName('image')[0].style['margin-top'] =  '-' + (bound.width/2) + 'px'
-
-      this.document.getElementsByClassName('image')[0].style['top'] = '50%'
-      this.document.getElementsByClassName('image')[0].style['left'] =  '50%'
-    }    
   }
 
 }
